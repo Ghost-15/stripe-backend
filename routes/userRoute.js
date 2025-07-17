3064428b5ab0583f14e8cbf12dfcec9856ff7db8
azat44 <azat4beats@gmail.com>
2025-07-17 19:48:31 +0200

    added email system with confirmation for user register and activation

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,15 +3,12 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 const verifyJWT = require('../middleware/verifyJWT')
 
-// router.use(verifyJWT)
+router.post('/activate', userController.activateAccount)
 
 router.route('/')
     .get(verifyJWT, userController.getAllUsers)
     .post(userController.createNewUser)
     .patch(verifyJWT, userController.updateUser)
     .delete(verifyJWT, userController.deleteUser)
-
-// router.route('/:id')
-//     .get(verifyJWT, userController.getUser);
 
 module.exports = router
