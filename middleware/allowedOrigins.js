28f83cd6ecbb31f4a9d6cf9ceb0b256d7fbdee1a
TimosLeBoss <tatibatchi15@hotmail.com>
2025-07-21 14:31:42 +0200

    Bien joue negro

--- middleware/allowedOrigins.js
+++ middleware/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://stripe-frontend-ll2x.onrender.com'
 ]
 
 module.exports = allowedOrigins
