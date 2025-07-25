71bf0d77e1c044bb12ead790332d7d6e9c635880
azat44 <160567934+azat44@users.noreply.github.com>
2025-07-25 20:36:50 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,48 +1,50 @@
 const { Model, DataTypes } = require("sequelize");
 const connection = require("../config/dbSequelize");
 
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {}
+class User extends Model {}
 
-  User.init(
-      {
-          first_name: {
-              type: DataTypes.STRING,
-              allowNull: false,
-              required: true
-          },
-          last_name: {
-              type: DataTypes.STRING,
-              allowNull: false,
-              required: true
-          },
-          username: {
-              type: DataTypes.STRING,
-              allowNull: false,
-              unique: true,
-              required: true,
-              validate: {
-                  isEmail: true,
-              },
-          },
-          password: {
-              type: DataTypes.STRING,
-              allowNull: false,
-              required: true,
-          },
-          roles: {
-              type: DataTypes.JSON,
-              allowNull: false,
-              required: true,
-          },
-          active:{
-              type: DataTypes.BOOLEAN,
-              defaultValue: false
-          }
-      },
-      {
-          sequelize: connection,
-      }
-  );
-  return User;
-}
+User.init(
+    {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            required: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true,
+            // validate: {
+            //     is: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,32}/,
+            // },
+        },
+        roles: {
+            type: DataTypes.JSON,
+            allowNull: false,
+            required: true,
+        },
+        active:{
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    },
+    {
+        sequelize: connection,
+    }
+);
+
+module.exports = User;
