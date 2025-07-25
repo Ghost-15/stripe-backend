6b318c012be651027fe3f76491fdba4d5b5b7535
TimosLeBoss <tatibatchi15@hotmail.com>
2025-07-25 23:51:31 +0200

    Bien joue negro

--- config/mailer.js
+++ config/mailer.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'tatibatchi15@gmail.com',
-        pass: 'bbvxpdtycblofjeo'
+        pass: 'bbvxpdtycblofjeoa'
     }
 });
 
