f6c880b302ab712fdde0d2c17451b51b75af5fc4
azat44 <160567934+azat44@users.noreply.github.com>
2025-07-25 20:36:34 +0200

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -1,65 +1,44 @@
-const { Model, DataTypes, Sequelize } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 const connection = require("../config/dbSequelize");
 
-module.exports = (sequelize, DataTypes) => {
-class Transaction extends Model {
-  static associate(models) {
-    Transaction.belongsTo(models.User, { foreignKey: "userId" });
-    Transaction.hasMany(models.Operation, { foreignKey: "transactionId" });
-  }
-}
+class transaction extends Model {}
 
-  Transaction.init(
-      {
-          userId: {
-              type: DataTypes.INTEGER,
-              allowNull: false,
-              references: {
-                  model: 'Users',
-                  key: 'id'
-              }
-          },
-          email: {
-              type: Sequelize.STRING,
-              allowNull: false
-          },
-          description:{
-              type: DataTypes.STRING,
-              allowNull: false,
-              required: true
-          },
-          amount:{
-              type: DataTypes.FLOAT,
-              allowNull: false,
-              required: true,
-          },
-          currency: { 
-              type: DataTypes.STRING, allowNull: false 
-          },
-          status:{
-              type: DataTypes.STRING,
-              allowNull: false,
-              defaultValue: 'waiting'
-          },
-          code:{
-              type: DataTypes.STRING,
-              allowNull: false,
-              required: true,
-              unique: true
-          },
-          stripeSessionId: {
-              type: DataTypes.STRING,
-              allowNull: true,
-          },
-          stripePaymentIntentId: {
-              type: DataTypes.STRING,
-              allowNull: true,
-          },
-      },
-      {
-          sequelize: connection,
-      }
-  );
-  
-  return Transaction;
-}
+transaction.init({
+
+            userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id'
+            }
+        },
+        
+        description:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true
+        },
+        amount:{
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            required: true,
+        },
+        status:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'waiting'
+        },
+        code:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true,
+            unique: true
+        }
+    },
+    {
+        sequelize: connection,
+    }
+);
+
+module.exports = transaction;
