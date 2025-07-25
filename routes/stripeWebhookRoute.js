811ba79699091a917afea2ef445906ac40649e97
azat44 <160567934+azat44@users.noreply.github.com>
2025-07-25 20:40:48 +0200

    Update stripeWebhookRoute.js

--- routes/stripeWebhookRoute.js
+++ routes/stripeWebhookRoute.js
@@ -1,7 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const stripeWebhookController = require('../controllers/stripeWebhookController');
 
-router.post('/webhook', express.raw({ type: 'application/json' }), stripeWebhookController.webhook);
-
-module.exports = router;
