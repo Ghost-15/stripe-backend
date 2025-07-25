c3556c1a8cb2c549314d3aaff94b00dfd5b0b1ef
azat44 <160567934+azat44@users.noreply.github.com>
2025-07-25 20:36:10 +0200

    Update marchand.js

--- models/marchand.js
+++ models/marchand.js
@@ -1,57 +1,51 @@
 const { Model, DataTypes } = require("sequelize");
 const connection = require("../config/dbSequelize");
 
-module.exports = (sequelize, DataTypes) => {
-  class Marchand extends Model {
-    static associate(models) {
-      Marchand.belongsTo(models.User, { foreignKey: "userId" });
-      models.User.hasOne(Marchand, { Marchand });
-    }
-  }
+class Marchand extends Model {}
 
-  Marchand.init(
-      {
-          userId: {
-              type: DataTypes.INTEGER,
-              allowNull: false,
-              references: {
-                  model: 'Users',
-                  key: 'id'
-              }
-          },
-          nomDeSociete:{
-              type: DataTypes.STRING,
-              allowNull: false,
-              required: true
-          },
-          adresse:{
-              type: DataTypes.STRING,
-              allowNull: false,
-              required: true,
-          },
-          numeroSiren:{
-              type: DataTypes.INTEGER,
-              allowNull: false,
-              required: true
-          },
-          code:{
-              type: DataTypes.INTEGER,
-              allowNull: false,
-              required: true
-          },
-          kbis:{
-              type: DataTypes.BLOB,
-              allowNull: false,
-              required: true
-          },
-          active:{
-              type: DataTypes.BOOLEAN,
-              defaultValue: false
-          }
-      },
-            {
-          sequelize: connection,
-      }
-  );
-  return Marchand;
-}
+Marchand.init(
+    {
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id'
+            }
+        },
+        nomDeSociete:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true
+        },
+        adresse:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true,
+        },
+        numeroSiren:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            required: true
+        },
+        code:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            required: true
+        },
+        kbis:{
+            type: DataTypes.BLOB,
+            allowNull: false,
+            required: true
+        },
+        active:{
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    },
+          {
+        sequelize: connection,
+    }
+);
+
+module.exports = Marchand;
