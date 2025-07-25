092e17030b33ba786362b1914bb2b450c623ba9f
azat44 <azat4beats@gmail.com>
2025-07-25 20:31:11 +0200

    get all user transaction using userId

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,36 +1,46 @@
-require('dotenv').config()
-const Transaction = require('../models/Transaction')
-const asyncHandler = require('express-async-handler')
-const { v4: uuidv4 } = require('uuid');
+    require('dotenv').config()
+    const Transaction = require('../models/Transaction')
+    const asyncHandler = require('express-async-handler')
+    const { v4: uuidv4 } = require('uuid');
 
 
-const getInfoTransaction = asyncHandler(async (req, res) => {
+    const getInfoTransaction = asyncHandler(async (req, res) => {
+        
+        const userId = req.user.id;
+        
+        const transactions = await Transaction.findAll({
+            where: {
+                userId: userId
+            }
+        });
+        
+        res.json(transactions);
+        
+    });
 
-})
 
+    const createNewTransaction = asyncHandler(async (req, res) => {
 
-const createNewTransaction = asyncHandler(async (req, res) => {
+        const { description, total } = req.body
+        const randomCode = uuidv4();
 
-    const { description, total } = req.body
-    const randomCode = uuidv4();
+        if ( !description || !total ) {
+            return res.status(422).json({message: 'All fields are required'})
+        }
 
-    if ( !description || !total ) {
-        return res.status(422).json({message: 'All fields are required'})
+        const transactionObject = { "description": description,
+            amount: total, code: randomCode }
+
+        // const transaction = await Transaction.create(transactionObject)
+
+        if (transactionObject){
+            return res.status(200).json({ message: '34513fe7-9128-418b-9622-f6c74c04fa1d' })
+        } else {
+            return res.status(400).json({ message: 'Not enregistrer' })
+        }
+    })
+
+    module.exports = {
+        getInfoTransaction,
+        createNewTransaction,
     }
-
-    const transactionObject = { "description": description,
-        amount: total, code: randomCode }
-
-    // const transaction = await Transaction.create(transactionObject)
-
-    if (transactionObject){
-        return res.status(200).json({ message: '34513fe7-9128-418b-9622-f6c74c04fa1d' })
-    } else {
-        return res.status(400).json({ message: 'Not enregistrer' })
-    }
-})
-
-module.exports = {
-    getInfoTransaction,
-    createNewTransaction,
-}
