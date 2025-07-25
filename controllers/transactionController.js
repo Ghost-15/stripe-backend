d67d2bec23e7f4bf54006ff7d4aa64ca7948ec33
TimosLeBoss <tatibatchi15@hotmail.com>
2025-07-25 23:55:59 +0200

    Bien joue negro

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 const Transaction = require('../models/Transaction')
 const asyncHandler = require('express-async-handler')
-const { v4: uuidv4 } = require('uuid');
 
 
 const getInfoTransaction = asyncHandler(async (req, res) => {
 
-})
+    const userId = req.user.id;
+
+    const transactions = await Transaction.findAll({
+        where: {
+            userId: userId
+        }
+    });
+
+    res.json(transactions);
+
+});
 
 module.exports = {
     getInfoTransaction,
