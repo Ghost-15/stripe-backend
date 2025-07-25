7b03ecf3df96bd58c515f4148f36d6bce5f0b5c4
azat44 <160567934+azat44@users.noreply.github.com>
2025-07-25 20:40:35 +0200

    Update stripeRoute.js

--- routes/stripeRoute.js
+++ routes/stripeRoute.js
@@ -1,7 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const stripeController = require('../controllers/stripeController');
 
-router.post('/pay', stripeController.createCheckoutSession);
-
-module.exports = router;
